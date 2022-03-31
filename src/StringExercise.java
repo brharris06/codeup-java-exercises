cfdb61a6b318d6a8d4029d0198616c40d3db9419
brharris06 <brandon.r.harris06@gmail.com>
2022-03-31 14:07:22 -0500

    Adding Java String Exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,5 @@
-public class StringExercise {
-    public static void main(String[] args) {
+//public class StringExercise {
+
 
 //        For each of the following output examples, create a string variable named message
 //        that contains the desired output and print it out to the console.
@@ -28,5 +28,6 @@ public class StringExercise {
         // String output4 = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\";
         // System.out.println(output4);
 
-    }
-}
+//    }
+//}
+
