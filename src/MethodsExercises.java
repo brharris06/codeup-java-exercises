2ee21abf96492e7657f7c689914d007a18d77e67
brharris06 <brandon.r.harris06@gmail.com>
2022-04-04 09:51:40 -0500

    Adding Methods Exercises, Java

---
+++ src/MethodsExercises.java
@@ -0,0 +1,45 @@
+public class MethodsExercises {
+        public static void main(String[] args) {
+            //Addition//
+
+//            int num1 = 10, num2 = 20, sum = 0;
+//            System.out.println("num1 = " + num1);
+//            System.out.println("num2 = " + num2);
+//            sum = num1 + num2;
+//            System.out.println("The sum = " + sum);
+
+            //Subtraction//
+
+//        int num1 = 20, num2 = 10, sub = 0;
+//        System.out.println("num1 = " + num1);
+//        System.out.println("num2 = " + num2);
+//        sub = num1 - num2;
+//        System.out.println("Subtraction = " + sub);
+
+            //Multiplication//
+
+//        int num1 = 20, num2 = 10, mult = 0;
+//        System.out.println("num1 = " + num1);
+//        System.out.println("num2 = " + num2);
+//        mult = num1 * num2;
+//        System.out.println("Multiplication = " + mult);
+
+            //Division//
+
+//        int num1 = 20, num2 = 10, div = 0;
+//        System.out.println("num1 = " + num1);
+//        System.out.println("num2 = " + num2);
+//        div = num1 / num2;
+//        System.out.println("Division = " + div);
+
+            //Modulus//
+
+//            int num1 = 5, num2 = 2, mod = 0;
+//            System.out.println("num1 = " + num1);
+//            System.out.println("num2 = " + num2);
+//            mod = num1 % num2;
+//            System.out.println("Remainder = " + mod);
+
+    }
+}
+
