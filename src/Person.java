759f41e1199915010895a248a02c25c5af5beea7
brharris06 <brandon.r.harris06@gmail.com>
2022-04-05 09:35:13 -0500

    Refactor code for persons file

--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,23 @@
-//public class Person {
-//    public String getName(){
-////TODO: return the person's name
-//    }
-//
-//    public void setName(String name){
-////TODO: change the name field to the passed value
-//    }
-//    public void sayHello(){
-////TODO: print a message to the console using the person's name
-//    }
-//}
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void sayHello(){
+        System.out.println(name + " says Hello!");
+    }
+
+    public String toString(){
+        return name;
+    }
+}
